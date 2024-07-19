365066b69759e4ff355ceef81fde7482931edefb
Vagarth <vagarth419@gmail.com>
2024-07-19 16:25:15 +0530

    feat: implement webrtc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing } from './components/Landing';
-import { Room } from './components/Room';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/room" element={<Room />} />
         <Route path="/" element={<Landing />} />
       </Routes>
     </BrowserRouter>
